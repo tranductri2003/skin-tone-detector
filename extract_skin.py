da419d647523736a2381aaacf12020562bc26866
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2024-01-14 15:56:29 +0700

    Add notebook and model

--- extract_skin.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import skin_detector
-
-img_path = input("Please Enter Image Path")
-image = cv2.imread(img_path)
-mask = skin_detector.process(image)
-cv2.imshow("input", image)
-cv2.imshow("mask", mask)
-cv2.waitKey(0)
